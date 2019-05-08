67683ad78a3ea8b1333912039ce0402997c6339f
ibesoft <ibesoft11@gmail.com>
2019-05-08 08:41:49 +0100

    login controller working

---
+++ controllers/login.js
@@ -0,0 +1,16 @@
+var authService = require('../services/authentication');
+
+class LoginController{
+    login(req, res, next){
+        var data = {
+            username: req.body.username,
+            password: req.body.password
+        };
+        authService.generateToken(data).then(token=>{
+            res.json(token);
+        }).catch(err=>{
+            res.json(err);
+        });
+    }
+}
+module.exports = new LoginController();
